f8496756368203d09a2942a4a83a2eec55aa9713
nourelmergawy <nouralmergawy@gmail.com>
2023-02-25 18:15:41 +0200

    nav home 2

--- app/src/main/java/com/example/graduationproject/ui/main/HomeActivity.kt
+++ app/src/main/java/com/example/graduationproject/ui/main/HomeActivity.kt
@@ -2,11 +2,30 @@ package com.example.graduationproject.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.example.graduationproject.HomeFragment
+import com.example.graduationproject.ProfileFragment
 import com.example.graduationproject.R
+import com.example.graduationproject.databinding.ActivityHomeBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class HomeActivity : AppCompatActivity() {
+    lateinit var navController: NavController
+    lateinit var binding :ActivityHomeBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment2) as NavHostFragment
+        val navController = navHostFragment.navController
+        binding.navigation.setupWithNavController(navController)
+
     }
+
+
 }
