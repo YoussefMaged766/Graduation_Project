d80a6fa4aac44b8da9af38455f2d6ebd4bd9b566
nourelmergawy <nouralmergawy@gmail.com>
2023-03-21 16:20:10 +0200

    book fragment

--- app/src/main/java/com/example/graduationproject/BookFragment.kt
+++ app/src/main/java/com/example/graduationproject/BookFragment.kt
@@ -1,6 +1,8 @@
 package com.example.graduationproject
 
+import android.content.ContentValues.TAG
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -8,20 +10,26 @@ import android.view.ViewGroup
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.stringPreferencesKey
+import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.navArgs
 import com.bumptech.glide.Glide
 import com.example.graduationproject.constants.Constants.Companion.dataStore
 import com.example.graduationproject.databinding.FragmentBookBinding
+import com.example.graduationproject.ui.main.favorite.FavoriteViewModel
+import com.example.graduationproject.ui.main.home.HomeViewModel
+import dagger.hilt.EntryPoint
+import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.launch
 
-
+@AndroidEntryPoint
 class BookFragment : Fragment() {
 
     lateinit var binding: FragmentBookBinding
     private lateinit var dataStore: DataStore<Preferences>
     private val data : BookFragmentArgs by navArgs()
+    val viewModel: FavoriteViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
@@ -45,18 +53,26 @@ class BookFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         show()
         selectHeart()
+        binding.imageViewHeart.setOnClickListener {
+
+        }
     }
 
     fun selectHeart(){
         binding.imageViewHeart.setOnClickListener {
             if (binding.imageViewAnimation.isSelected){
                 binding.imageViewAnimation.isSelected = false
+
             }else{
                 binding.imageViewAnimation.isSelected = true
                 binding.imageViewAnimation.likeAnimation()
+                lifecycleScope.launch {
+                    addFavoriate()
+                }
             }
         }
     }
+
 
     fun show(){
         Glide.with(requireContext()).load(data.bookObject.coverImage).into(binding.BookImage)
@@ -71,5 +87,12 @@ class BookFragment : Fragment() {
         val dataStoreKey: Preferences.Key<String> = stringPreferencesKey(key)
         val preference = dataStore.data.first()
         return preference[dataStoreKey]
+    }
+    private suspend fun addFavoriate(){
+        viewModel.addFavorite(
+            getToken("userToken").toString(),
+            data.bookObject.bookId.toString()
+        )
+        Log.d(TAG, "addFavoriate: i'm here")
     }
 }
--- app/src/main/java/com/example/graduationproject/utils/WebServices.kt
+++ app/src/main/java/com/example/graduationproject/utils/WebServices.kt
@@ -2,13 +2,11 @@ package com.example.graduationproject.utils
 
 
 import com.example.graduationproject.models.*
-import retrofit2.Response
 import retrofit2.http.Body
-import retrofit2.http.FormUrlEncoded
 import retrofit2.http.GET
 import retrofit2.http.Header
-import retrofit2.http.Headers
 import retrofit2.http.POST
+import retrofit2.http.PUT
 import retrofit2.http.Path
 import retrofit2.http.Query
 
@@ -36,5 +34,7 @@ interface WebServices {
     @GET("book")
     suspend fun getAllBooks(@Header("token") token:String,@Query("page") page:Int): BookResponse
 
+    @PUT("list/addToFavorits")
+    suspend fun addFavoirate(@Header("token") token:String, @Body book: String)
 
 }
